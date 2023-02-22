8dc237f43f9966eecf5f2e25070e3ba0fd421748
neeraj <neerajpenumaka7@gmail.com>
2023-02-22 18:14:37 +0530

    initial commit

---
+++ compression/load_data.py
@@ -0,0 +1,76 @@
+from glob import glob
+from tqdm import tqdm
+import imageio
+import numpy as np
+import cv2
+import torch 
+import torch.utils as utils
+import os
+import torch.utils.data as utils
+from preprocessing import preprocessing
+
+"""
+Data Loader function handles both scenarios where UE-DETRAC dataset is 
+true or not. Data must have a clearly sequential naming convention to 
+ensure that frames are stored in the same order in the folder as they are 
+in the video. e.g frame_1 --> frame_00001 when there are less than 10000 
+frames. Failure to do so will result in frames misordering such as 
+frame_1,frame_10 appearing next to one another
+
+Inputs: 
+    - path : path to the datasets files, 
+    - detrac : flag to use UE-DETRAC data, Fasle by default
+    - train = False by default, True is training, Fasle if testing
+
+Output: 
+    - train_loader : The dataloader 
+    - frames : A list a numpy arrays
+    
+"""
+def load_data(path, detrac = False, train = False):
+
+    
+    data = []
+    #detrac had an unconventional naming sequence that needs to be accounted seperately so as to not mix up the frame order
+    #note that this was simply for testing functionality on the first video in detrac
+    
+    print( '\n Loading Data ..... ')
+    frames = []
+    if detrac:
+        print( '\n DETRAC .... ')
+        c = 0
+        for i in os.listdir('./../datasets/Insight-MVT_Annotation_Train/'):
+            n_data = []
+            c+=1
+            for j in tqdm(range(1, len(os.listdir('./../datasets/Insight-MVT_Annotation_Train/' + i + '/'))+1)):
+                j = str(j)
+                jj= 5-len(j)
+                k = "img" + jj*"0" +j +".jpg"
+                img = imageio.imread('./../datasets/Insight-MVT_Annotation_Train/' + i + '/' + k + '/')
+                #print("image", img)
+                n_data.append(cv2.resize(img, (100, 100), interpolation = cv2.INTER_AREA))
+            n_data = np.array(n_data)
+            data = n_data.copy()
+            print()
+            break
+        
+    else:
+        frames = preprocessing(path)
+        for img in tqdm(frames):
+            data.append(cv2.resize(img, (100, 100), interpolation = cv2.INTER_AREA))
+                         
+    data_array = np.array(data)
+    print(data_array.shape)
+    data_load = data_array.transpose(0,3,1,2)
+    data_load= (data_load - 255) / 255
+
+    batch_size = 8
+
+    print( ' \n Creating dataloader ..')
+    
+    tensor_x = torch.stack([torch.Tensor(i) for i in data_load])
+
+    train_dataset = utils.TensorDataset(tensor_x,tensor_x)
+    train_loader = utils.DataLoader(train_dataset, batch_size=batch_size, shuffle = train)
+
+    return train_loader, frames
---
+++ compression/train.py
@@ -0,0 +1,76 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torchvision as tv
+import torch.optim as optim
+import torch.utils.data as utils
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+import torchvision.datasets as dset
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+from load_data import load_data
+from CAE import CAE
+
+'''
+A function that is used to train the model for a specified number of epochs
+
+Inputs : 
+
+    -path : to call the load_data function that returns the dataloader
+    -DETRAC : True if being tested on DETRAC ( for development only )
+    -train : 
+    -epoch : Number of epochs to train for
+    -verbose : True if loss is to be displayed at each epoch
+    
+Outputs : None
+'''
+
+def train(path,epoch,verbose=False,DETRAC=False):
+
+    print('Training using DETRAC: ', DETRAC)
+    #Dataloading
+    train_loader, _ = load_data(path,DETRAC,True)
+    images, _ = next(iter(train_loader))  
+    
+    # Load pre-trained model 
+    model_n = CAE()
+    #model_n.load_state_dict(torch.load("./../models/CAE_Full_data.pwf"))
+
+    """Use this for training using GPU"""
+    model_n.cuda()
+    
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model_n.parameters(), lr=0.0001)
+
+    #Function that runs one epoch
+    def training(model):
+        
+        model.train()
+
+        for batch_idx, batch in enumerate(tqdm(train_loader)):
+            torch.cuda.empty_cache()
+            images, targets = batch[0], batch[0]
+            images, targets = images.cuda(), targets.cuda()
+
+            output = model(images)
+
+            loss = criterion(output, targets)
+            loss = torch.sqrt(loss)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+        return loss, model
+    
+    #Training loop 
+    print('\nTraining ..\n')
+    for e in range(epoch):
+        loss, model = training(model_n)
+        torch.save(model.state_dict(), "./../models/CAE_Full_data.pwf")
+        if verbose : print("Epoch : ",e+1, " - Loss : ",loss)
+        print()
